2f2bf0a32bcbbd82c6553191126484407516befe
Ejerson adarlo Beloso <belosoej@gmail.com>
2022-10-19 23:48:14 +0800

    first commit

---
+++ server/controller/blogController.js
@@ -0,0 +1,52 @@
+const asyncHandler = require("express-async-handler");
+
+const Blog = require("../models/blogs");
+
+const getBlogs = asyncHandler(async (req, res) => {
+  const blogs = await Blog.find({ user: req.user.id });
+
+  res.status(200).json(blogs);
+});
+
+const setBlog = asyncHandler(async (req, res) => {
+  const blog = await Blog.create({
+    user: req.user.id,
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  res.status(200).json(blog);
+});
+
+const updateBlog = asyncHandler(async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+
+  if (!blog) {
+    res.status(400);
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedBlog);
+});
+
+const deleteBlog = asyncHandler(async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+
+  if (!blog) {
+    res.status(400);
+  }
+
+  await blog.remove();
+
+  res.status(200).json({ id: req.params.id });
+});
+
+module.exports = {
+  getBlogs,
+  setBlog,
+  updateBlog,
+  deleteBlog,
+};
---
+++ server/routes/blogsRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getBlogs,
+  setBlog,
+  updateBlog,
+  deleteBlog,
+} = require("../controller/blogController");
+
+router.route("/").get(getBlogs).post(setBlog);
+router.route("/:id").delete(deleteBlog).put(updateBlog);
+
+module.exports = router;
